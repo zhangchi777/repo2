3c9134dc18072b1dbe090a64bec831e92b8bc349
zhangchi <zhangchi@itcast.cn>
2018-10-14 21:25:28 +0800

    2

--- git_idea/src/com/itheima/hello.java
+++ git_idea/src/com/itheima/hello.java
@@ -7,11 +7,13 @@ public class hello {
         System.out.println("hello===========================111");
         System.out.println("hello===========================111");
         System.out.println("hello===========================111");
+        System.out.println("33333333333333");
     }
 
     public void  test(){
         System.out.println("hello==========22222222222222");
         System.out.println("hello==========22222222222222");
         System.out.println("hello==========22222222222222");
+        System.out.println("3333333333333333333");
     }
 }
